9ab69fad7ac49c8425fdc44aa76e648c6a3056c4
angelbandera0 <angelbandera0@gmail.com>
2021-05-17 09:50:10 +0200

    Ajustes para los likes

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -3,22 +3,29 @@ const { User, Casa, Like } = require("../models");
 
 //agrega un like
 const addLike = async (req = request, res = response) => {
-  const { idCasa, idUser } = req.body;
-
-  //busca el user y la planta
-  const [casa, user] = await Promise.all([
-    Casa.findById(idCasa),
-    User.findById(idUser),
-  ]);
-  //registra el nuevo like
-  const newLike = new Like({ casa, user });
-  const resp = await newLike.save();
-  //actualiza los likes del user y la planta
-  casa.likes.push(resp);
-  user.myLikes.push(resp);
-  Promise.all([casa.save(), user.save()]);
-  //retorna los valores
-  res.status(200).send({ like: resp });
+  try {
+    const { idCasa } = req.body;
+    const idUser = req.usuario._id;
+    //busca el user y la planta
+    const [casa, user] = await Promise.all([
+      Casa.findById(idCasa),
+      User.findById(idUser),
+    ]);
+    //registra el nuevo like
+    console.log(casa);
+    const newLike = new Like({ casa, user });
+    const resp = await newLike.save();
+    //console.log(resp);
+    //actualiza los likes del user y la planta
+    casa.likes.push(resp);
+    user.myLikes.push(resp);
+    await Promise.all([casa.save(), user.save()]);
+    //retorna los valores
+    res.status(200).send({ like: resp });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send({ msg: "Ha ocurrido un error a la hora de dar un like" });
+  }
 };
 
 //elimina el like
@@ -26,7 +33,7 @@ const removeLike = async (req = request, res = response) => {
   const { id } = req.params;
   //elimina el like
   const resp = await Like.findByIdAndDelete(id);
-  
+
   //busca la planta y el user q tenia ese like
   const casa = await Casa.findById(resp.casa);
   const user = await User.findById(resp.user);
@@ -35,7 +42,7 @@ const removeLike = async (req = request, res = response) => {
   casa.likes.pull(resp);
   user.myLikes.pull(resp);
   Promise.all([casa.save(), user.save()]);
-  
+
   //retorna los valores
   res.status(200).send({ like: resp });
 };
