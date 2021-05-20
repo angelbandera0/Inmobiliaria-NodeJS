873510f81d07c9f28ab77acc0b23298face9c327
angelbandera0 <angelbandera0@gmail.com>
2021-05-20 16:08:45 -0400

    Install Compression

--- app.js
+++ app.js
@@ -1,61 +1,65 @@
 //envairoments var
-require('dotenv').config();
+require("dotenv").config();
 
 //var
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fileUpload = require('express-fileupload');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const fileUpload = require("express-fileupload");
+const compression = require("compression");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const buscarRouter = require('./routes/buscar');
-const casasRouter = require('./routes/casas');
-const authRouter = require('./routes/auth');
-const solicitudRouter = require('./routes/solicitud');
-const likesRouter = require('./routes/likes');
-const citasRouter = require('./routes/cita');
-const emailRouter = require('./routes/email');
+//import routes
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const buscarRouter = require("./routes/buscar");
+const casasRouter = require("./routes/casas");
+const authRouter = require("./routes/auth");
+const solicitudRouter = require("./routes/solicitud");
+const likesRouter = require("./routes/likes");
+const citasRouter = require("./routes/cita");
+const emailRouter = require("./routes/email");
 
-const { dbConnection } = require('./database/config');
-const { initRolesDB } = require('./controllers/roleController');
+//import db
+const { dbConnection } = require("./database/config");
+const { initRolesDB } = require("./controllers/roleController");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //middlewares
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // compress all responses
+app.use(express.static(path.join(__dirname, "dist"))); //angular production
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, "dist")));
 // Fileupload - Carga de archivos
-app.use( fileUpload({
-  useTempFiles : true,
-  tempFileDir : '/tmp/',
-  createParentPath: true
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+    createParentPath: true,
+  })
+);
 
 //routes
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/casa', casasRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/solicitud', solicitudRouter);
-app.use('/api/buscar', buscarRouter);
-app.use('/api/like', likesRouter);
-app.use('/api/cita', citasRouter);
-app.use('/api/email', emailRouter);
-
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/casa", casasRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/solicitud", solicitudRouter);
+app.use("/api/buscar", buscarRouter);
+app.use("/api/like", likesRouter);
+app.use("/api/cita", citasRouter);
+app.use("/api/email", emailRouter);
 
 // Conectar a la DB
 dbConnection();
