f26cb49fd1ca7a713549b0ee5ecff1efc52918a9
angelbandera0 <angelbandera0@gmail.com>
2021-05-22 00:32:49 -0400

    Se implementó el metodo de enviar mensaje de confirmación

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -13,7 +13,7 @@ const send = (req, res = response) => {
   // Definimos el email
   var mailOptions = {
     from: process.env.GMAIL_USER,
-    to: "banderaangel971114@gmail.com",
+    to: "jorgedelatorredelarosa@gmail.com",
     subject: "Asunto",
     text: "Contenido del email",
   };
@@ -28,8 +28,42 @@ const send = (req, res = response) => {
     }
   });
 };
+//recive el email destinatario y el objeto token
+const sendConfirm = (req,user, token) => {
+  // Definimos el transporter
+  var transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+  // Definimos el email
+  var mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: user.email,
+    subject: "Token de Verificación de Cuenta",
+    text:
+      "Hola "+user.name+",\n\n" +
+      "Por favor verifica tu cuenta dondo haciendo click sobre este link: \nhttp://" +
+      req.headers.host +
+      "/api/auth/confirmation/" +
+      token.token +
+      ".\n",
+  };
+  // Enviamos el email
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      throw "No se ha podido enviar el email.";
+    } else {
+      console.log("Email enviado satisfactoriamente.");
+      return token;
+    }
+  });
+};
 
 module.exports = {
   send,
+  sendConfirm,
 };
-
