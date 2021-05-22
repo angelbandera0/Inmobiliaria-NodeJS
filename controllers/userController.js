e8680481c566a5e6f97c6fcaa4648c8cc01611f8
angelbandera0 <angelbandera0@gmail.com>
2021-05-22 00:36:32 -0400

    Se modificó el metodo userPost

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,12 @@
 const { request, response } = require("express");
 const bcryptjs = require("bcryptjs");
-const { subidaImagenCloudinary, actualizarImagenCloudinary, eliminarImagenCloudinary } = require("./subidasController");
-const { User,Rol } = require('../models');
+const {
+  subidaImagenCloudinary,
+  actualizarImagenCloudinary,
+  eliminarImagenCloudinary,
+} = require("./subidasController");
+const { sendConfirm } = require("./emailController");
+const { User, Rol, Token } = require("../models");
 
 //obtener listado de usuarios de forma paginada
 //limite y desde son parametros pasados por la url
@@ -9,10 +14,7 @@ const userGet = async (req = request, res = response) => {
   const { limite = 5, desde = 0 } = req.query;
   const [total, users] = await Promise.all([
     User.countDocuments(),
-    User.find()
-        .populate("role")
-        .skip(Number(desde))
-        .limit(Number(limite)),
+    User.find().populate("role").skip(Number(desde)).limit(Number(limite)),
   ]);
 
   res.status(200).send({
@@ -21,11 +23,10 @@ const userGet = async (req = request, res = response) => {
   });
 };
 
-
 const userGetById = async (req = request, res = response) => {
   const { id } = req.params;
   const user = await User.findById(id);
-  
+
   res.status(200).send({
     user: user,
   });
@@ -34,44 +35,49 @@ const userGetById = async (req = request, res = response) => {
 const misAgregaciones = async (req = request, res = response) => {
   const { id } = req.params;
   const user = await User.findById(id)
-                         .populate('solicitudes')
-                         .populate('casas')
-                         .populate('citas');
+    .populate("solicitudes")
+    .populate("casas")
+    .populate("citas");
 
-  
   res.status(200).send({
     user,
     userSolicitudes: user.solicitudes ? user.solicitudes : [],
     userCasas: user.casas ? user.casas : [],
-    userCitas: user.citas ? user.citas : [],        
+    userCitas: user.citas ? user.citas : [],
   });
-}
-
+};
 
 //Agregar Usuario
 const userPost = async (req, res = response) => {
-  const { name, password, email} = req.body;
+  const { name, password, email } = req.body;
   console.log(req);
   try {
-    
     //Subir la imagen al Cloudinary
-    const urlImage = await subidaImagenCloudinary( req.files.archivo);
+    const urlImage = await subidaImagenCloudinary(req.files.archivo);
     const image = urlImage;
-    
+
     const user = new User({ name, password, email, image });
-    
+
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
     user.password = bcryptjs.hashSync(password, salt);
-    
+
     // Guardar en DB
     const resU = await user.save();
     //Buscar el Rol en la DB
     const resRol = await Rol.findById(resU.rol);
     //Asignar el user al array de user del rol
     resRol.users.push(user);
-    //guardar cambios en el rol 
-    await resRol.save();  
+    //guardar cambios en el rol
+    await resRol.save();
+
+    // Create a verification token for this user
+    const token = new Token({
+      userId: resU,
+      token: bcryptjs.hashSync(`${name}${Date.now()}`, salt),
+    });
+    const resToken = await token.save();
+    sendConfirm(req, resU, token);
 
     res.status(201).send({
       msg: "Usuario creado correctamente",
@@ -86,7 +92,7 @@ const userPost = async (req, res = response) => {
 //actualiza un usuario
 const userPut = async (req, res = response) => {
   const { id } = req.params;
-  const { _id, password, email, createdAt,rol, ...resto } = req.body;
+  const { _id, password, email, createdAt, rol, ...resto } = req.body;
 
   try {
     if (password) {
@@ -100,16 +106,18 @@ const userPut = async (req, res = response) => {
     const user = await User.findById(id);
 
     //Actualizar imagen en Cloudinary
-    const urlImg = await actualizarImagenCloudinary(req.files.archivo,user.image);
-    resto.image=urlImg;
+    const urlImg = await actualizarImagenCloudinary(
+      req.files.archivo,
+      user.image
+    );
+    resto.image = urlImg;
 
     //actualizar usuario
     await user.update(resto);
 
-
     //Buscar y actualizar
     //await User.findByIdAndUpdate(id, resto);
-    
+
     res.status(200).send({ msg: "User Actualizado Correctame" });
   } catch (e) {
     res.status(400).send({ msg: "Ha ocurrido un error en la actualizacón" });
@@ -119,20 +127,25 @@ const userPut = async (req, res = response) => {
 //elimina un usuario
 const userDelete = async (req, res = response) => {
   const { id } = req.params;
-  
+
   try {
-    
     //Fisicamente lo borramos
     const resp = await User.findByIdAndRemove(id);
-    
-    //Eliminamos la imagen de Cloudinary  
+
+    //Eliminamos la imagen de Cloudinary
     eliminarImagenCloudinary(resp.image);
 
     res.status(200).send({ msg: "Usuario eliminado Correctamente" });
-  
   } catch (e) {
     res.status(400).send({ msg: "Ha ocurrido un error en la eliminación" });
   }
 };
 
-module.exports = { userPost, userGet, userPut, userDelete,userGetById, misAgregaciones };
+module.exports = {
+  userPost,
+  userGet,
+  userPut,
+  userDelete,
+  userGetById,
+  misAgregaciones,  
+};
