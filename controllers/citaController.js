8a634f59c19c2a64857ea381b1faa174709333d4
jldelatorre <jorgedelatorredelarosa@gmail.com>
2021-05-26 00:08:52 -0400

    Agregado metodo citaConfirm

--- controllers/citaController.js
+++ controllers/citaController.js
@@ -1,87 +1,111 @@
 const { request, response } = require("express");
 const { User, Casa, Cita } = require("../models");
-
+const { sendConfirm } = require("./emailController");
 
 const citaGet = async (req = request, res = response) => {
-    const [total, citas] = await Promise.all([
-      Cita.countDocuments(),
-      Cita.find().populate('casa')
+  const { estado } = req.query;
+  const [total, citas] = await Promise.all([
+    Cita.countDocuments({ estado: estado }),
+    Cita.find({ estado: estado }).sort({ createdAt: -1 }).populate("casa").populate("user"),
+  ]);
+
+  res.status(200).send({
+    total,
+    citas,
+  });
+};
+
+const citaGetById = async (req = request, res = response) => {
+  const { id } = req.params;
+  const cita = await Cita.findById(id);
+
+  res.status(200).send({
+    cita,
+  });
+};
+
+const citaPost = async (req = request, res = response) => {
+  const { idCasa } = req.body;
+  const idUser = req.usuario._id;
+
+  try {
+    const [casa, user] = await Promise.all([
+      Casa.findById(idCasa),
+      User.findById(idUser),
     ]);
-  
-    res.status(200).send({
-        total,
-        citas    
-    });
-  };
+    //registra la nueva cita
+    const newCita = new Cita({ casa, user });
+    const resp = await newCita.save();
+    //console.log(resp);
+    //actualiza las citas del user y la casa
+    casa.citas.push(resp);
+    user.citas.push(resp);
+    console.log("fgsdddddddshdshshhhhhhhhhtrrthwhtrw");
+    await casa.save();
+    await user.save();
+    //retorna los valores
+    res.status(200).send({ cita: resp });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(400)
+      .send({ msg: "Ha ocurrido un error a la hora de crear la cita", e });
+  }
+};
 
+const citaPut = async (req = request, res = response) => {
+  const { id } = req.params;
+  const { _id, ...data } = req.body;
+  try {
+    console.log(id);
+    const cita = await Cita.findByIdAndUpdate(id, data);
 
-  const citaGetById = async (req = request, res = response) => {
-    const { id } = req.params;
+    res.status(200).send({ cita });
+  } catch (e) {
+    res
+      .status(400)
+      .send({ msg: "Ha ocurrido un error a la hora de actualizar la cita", e });
+  }
+};
+
+const citaDelete = async (req = request, res = response) => {
+  const { id } = req.params;
+  try {
+    cita = await Cita.findByIdAndDelete(id);
+
+    res.status(200).send({ msg: "Casa eliminada correctamente" });
+  } catch (e) {
+    res.status(400).send({ msg: "Error", e });
+  }
+};
+
+const citaConfirm = async (req = request, res = response) => {
+  const { subject, text, fecha } = req.body;
+  const { id } = req.params;
+
+  try {
     const cita = await Cita.findById(id);
-  
-    res.status(200).send({
-      cita
-    });
-  };
+    cita.estado = "Aprobada";
+    cita.fecha = fecha;
+    await cita.save;
 
-  const citaPost = async (req = request, res = response) => {
-    const { idCasa } = req.body;
-    const idUser = req.usuario._id;
+    const cuerpoCorreo = {
+      subject: subject,
+      text: text,
+    };
+    sendConfirm(req.usuario, cuerpoCorreo);
 
-    try{
-        const [casa, user] = await Promise.all([
-            Casa.findById(idCasa),
-            User.findById(idUser),
-          ]);
-          //registra la nueva cita
-        const newCita = new Cita({ casa, user });
-        const resp = await newCita.save();
-        //console.log(resp);
-        //actualiza las citas del user y la casa
-        casa.citas.push(resp);
-        user.citas.push(resp);
-        console.log("fgsdddddddshdshshhhhhhhhhtrrthwhtrw");
-        await casa.save(); 
-        await user.save();
-        //retorna los valores
-        res.status(200).send({ cita: resp });
-    } catch (e) {
-        console.log(e);
-        res.status(400).send({ msg: "Ha ocurrido un error a la hora de crear la cita", e });
-    }
+    res.status(200).send({ msg: "Lacita ha sido agendada" });
+  } catch (e) {
+    res.status(400).send({ msg: "Error", e });
   }
+};
 
-  const citaPut = async (req = request, res = response) => {
-    const { id } = req.params;
-    const { _id, ...data } = req.body;
-    try{
-        console.log(id);
-        const cita = await Cita.findByIdAndUpdate(id, data);
-
-        res.status(200).send({cita});
-
-    }catch(e){
-        res.status(400).send({ msg: "Ha ocurrido un error a la hora de actualizar la cita", e });
-    }
-  }
-
-  const citaDelete = async (req = request, res = response) => {
-      const { id } = req.params;
-    try{
-      cita = await Cita.findByIdAndDelete(id);
-    
-      res.status(200).send({ msg : "Casa eliminada correctamente"});
-    }catch(e){
-        res.status(400).send({ msg : "Error", e});
-
-    }
-
-  }
-    
-  module.exports = { 
-      citaGet,
-      citaGetById,
-      citaPost,
-      citaPut,
-      citaDelete
-  }
+module.exports = {
+  citaGet,
+  citaGetById,
+  citaPost,
+  citaPut,
+  citaDelete,
+  citaConfirm,
+};
