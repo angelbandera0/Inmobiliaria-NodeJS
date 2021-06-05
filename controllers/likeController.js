17beca8d6acbe266043102f92e6c5b87dd055f69
angelbandera0 <angelbandera0@gmail.com>
2021-06-05 10:58:41 -0400

    Modificación del remover like y su tratamiento de excepciones

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -17,9 +17,9 @@ const addLike = async (req = request, res = response) => {
     //update userLikes and casaLikes
     const [a, b] = await Promise.all([
       User.updateOne({ _id: idUser }, { $push: { myLikes: resp._id } }),
-      await Casa.updateOne({ _id: idUser }, { $push: { likes: resp._id } }),
+      await Casa.updateOne({ _id: idCasa }, { $push: { likes: resp._id } }),
     ]);
-    
+
     //envio de respuesta
     res.status(200).send({ like: resp });
   } catch (e) {
@@ -32,23 +32,33 @@ const addLike = async (req = request, res = response) => {
 
 //elimina el like
 const removeLike = async (req = request, res = response) => {
-  const { id } = req.params;
-  //elimina el like
-  const resp = await Like.findByIdAndDelete(id);
+  try {
+    const { id } = req.params;
+    //elimina el like
+    const resp = await Like.findByIdAndDelete(id);
 
-  //busca la planta y el user q tenia ese like
-  const casa = await Casa.findById(resp.casa);
-  const user = await User.findById(resp.user);
+    //actualiza el dichos modelos retirando el like q fue eliminado
+    Promise.all([
+      Casa.updateOne(
+        { _id: resp.casa._id },
+        { $pull: { likes: mongoose.Types.ObjectId(id) } },
+        { multi: true }
+      ),
+      User.updateOne(
+        { _id: resp.user._id },
+        { $pull: { myLikes: mongoose.Types.ObjectId(id) } },
+        { multi: true }
+      ),
+    ]);
 
-  //actualiza el dichos modelos retirando el like q fue eliminado
-  casa.likes.pull(resp);
-  user.myLikes.pull(resp);
-  await casa.save();
-  await user.save();
-  //Promise.all([casa.save(), user.save()]);
-
-  //retorna los valores
-  res.status(200).send({ like: resp });
+    //retorna los valores
+    res.status(200).send({ like: resp });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(400)
+      .send({ msg: "Ha ocurrido un error a la hora de remover el like." });
+  }
 };
 
 module.exports = {
